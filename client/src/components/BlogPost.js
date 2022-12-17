6b62e9f59c9d45a053e2bc6038c3b36e45023b77
Heneros <punk60826@gmail.com>
2022-12-18 01:44:43 +0200

    blogpost information display

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -9,12 +9,11 @@ export default function BlogPost({ post }) {
 
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={imageUrl} />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    {description}
                 </Card.Text>
                 <Button variant="primary">Visit Page</Button>
             </Card.Body>
