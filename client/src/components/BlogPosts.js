c0a49890ba3fddfa81b2e37842cee84a52b41c5c
Heneros <punk60826@gmail.com>
2023-01-26 00:01:26 +0200

    updated homepage.js

--- client/src/components/BlogPosts.js
+++ client/src/components/BlogPosts.js
@@ -1,24 +1,48 @@
 import React from 'react'
 import { gql, useQuery, useSubscription } from '@apollo/client';
 import { GET_POSTS } from '../graphql/queries';
-
+import axios from '../axios';
+import { useParams } from 'react-router-dom';
 
 import BlogPost from './BlogPost';
+import { useState } from 'react';
 
 
 
 
 function BlogPosts() {
 
-  const { data, loading, error } = useQuery(GET_POSTS);
-  if (loading) { return <p>Loading...</p> }
-  if (error) { return `Error! ${error.message}` }
+  const { id } = useParams();
+
+  const [data, setData] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+
+  React.useEffect(() => {
+    axios
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.warn(err + `Error`);
+        // alert(`Error`);
+      }, [])
+  });
+
+
+
+  // const { data, loading, error } = useQuery(GET_POSTS);
+  // if (loading) { return <p>Loading...</p> }
+  // if (error) { return `Error! ${error.message}` }
 
   return (
     <>
-      {data.posts.map(post => (
+ 
+      {/* {data.posts.map(post => (
         <BlogPost key={post.id} post={post} />
-      ))}
+      ))} */}
 
     </>
   )
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,21 +1,42 @@
 import React, { useEffect, useState } from 'react'
-import BlogPosts from '../components/BlogPosts'
-import Header from '../components/Header'
-import CreatePost from '../components/CreatePost'
+import { useDispatch, useSelector } from 'react-redux';
+
+
+import BlogPosts from '../components/BlogPosts';
+import Header from '../components/Header';
+import CreatePost from '../components/CreatePost';
+import { fetchPosts } from '../redux/slices/posts';
+
+
 export default function HomePage() {
+    const dispatch = useDispatch();
+    const { posts } = useSelector(state => state.posts);
+    const isPostsLoading = posts.status === 'loading';
+
+    React.useEffect(() => {
+        dispatch(fetchPosts());
+    }, []);
+    console.log(posts);
 
     return (
         <>
             <Header />
             <div className='container'>
-
                 <div className="row">
                     <div className="col-xl-8">
-                        <BlogPosts />
+                        {(isPostsLoading ? [...Array(1)] : posts.items).map((obj, index) => isPostsLoading ? (
+                            ////  <span>Posts not found.</span>
+                            <BlogPosts
+                                key={index}
+                            />
+                        ) : (
+                            <BlogPosts
+                                id={obj._id}
+                            />
+                        )
+                        )};
                     </div>
                     <div className="col-xl-4">
-                        {/* You clicked {count} times. */}
-
                         <div className="btns">
                             <CreatePost />
                         </div>
