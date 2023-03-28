836e95c3709ae7fa5e5afae73643b684f6e26c6e
Heneros <punk60826@gmail.com>
2023-03-29 02:02:17 +0300

    CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,8 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../components/Header';
+import { selectIsAuth } from '../redux/slices/auth';
 
 
 export default function CreatePost() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const isAuth = useSelector(selectIsAuth);
+    const [text, setText] = useState('');
+    const [title, setTitle] = useState('');
+    const [tags, setTags] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    const inputFileRef = useRef(null);
+    const isEditing = Boolean(id);
+
+    const handleChangeFile = async (event) => {
+
+    };
     return (
         <>
             <Header />
