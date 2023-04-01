83b9a39a805347328e8c38ae20c1f1994afa9af0
Heneros <punk60826@gmail.com>
2023-04-01 03:19:01 +0300

    CreatePost.js updated

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import { selectIsAuth } from '../redux/slices/auth';
 import { Button } from '@mui/material';
+import axios from '../axios';
 
 
 export default function CreatePost() {
@@ -22,11 +23,37 @@ export default function CreatePost() {
         try {
             const formData = new FormData();
             const file = event.target.files[0];
-
+            formData.append('image', file);
+            const { data } = await axios.post('/upload', formData);
+            setImageUrl(data.url); //сохранение в переменной setImageUrl
         } catch (err) {
-
+            console.warn(err);
         }
     };
+
+    useEffect(() => {
+        if (id) {
+            axios
+                .get(`/posts/${id}`).then(({ data }) => {
+                    setTitle(data.title);
+                    setText(data.text);
+                    setImageUrl(data.imageUrl);
+                    setTags(data.tags).join(',');
+                })
+                .catch((err) => {
+                    console.warn(err);
+                })
+        }
+    }, []);
+
+    const onClickRemoveImage = () => {
+        setImageUrl('');
+    }
+
+    const onChange = useCallback((value) => {
+        setText(value)
+    }, []);
+    
     return (
         <>
             <Header />
